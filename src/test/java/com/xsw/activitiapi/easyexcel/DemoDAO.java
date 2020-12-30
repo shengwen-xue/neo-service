30472592fb935a0e41e7804d2e5ed98f800f6ae2
xueshengwen <xueshengwen@aliyun.com>
2020-12-30 09:52:04 +0800

    EasyExcel测试

--- src/test/java/com/xsw/activitiapi/easyexcel/DemoDAO.java
+++ src/test/java/com/xsw/activitiapi/easyexcel/DemoDAO.java
@@ -20,5 +20,8 @@ public class DemoDAO {
         String fileName = "D:\\my-doc\\1.xlsx";
         // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
         EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).sheet().doRead();
+
+        // EasyExcel.write().sheet().doWrite();
+        // ExcelUtil.writeBySimple(filePath,data,head);
     }
 }
