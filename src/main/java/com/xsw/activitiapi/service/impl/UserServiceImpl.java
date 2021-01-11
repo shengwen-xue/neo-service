4da48824fe08d224564f49c968abc83cb6033ecb
xueshengwen <xueshengwen@aliyun.com>
2021-01-11 18:08:03 +0800

    修改查数据库的代码

--- src/main/java/com/xsw/activitiapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/xsw/activitiapi/service/impl/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package com.xsw.activitiapi.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.xsw.activitiapi.mapper.UserMapper;
 import com.xsw.activitiapi.model.dto.UserDTO;
+import com.xsw.activitiapi.model.entity.User;
 import com.xsw.activitiapi.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -24,6 +28,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<UserDTO> getUserInfo(String userId) {
-        return userMapper.getUserInfo(userId);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id", userId);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        return userList.stream().map(user -> {
+            UserDTO userDTO = new UserDTO();
+            BeanUtils.copyProperties(user, userDTO);
+            return userDTO;
+        }).collect(Collectors.toList());
+        // return userMapper.getUserInfo(userId);
     }
 }
