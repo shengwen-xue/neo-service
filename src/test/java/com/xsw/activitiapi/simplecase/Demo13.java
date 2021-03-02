a47acc4a3bcf86d07f7660c4bbe2a3b8b4e525d7
xueshengwen <xueshengwen@aliyun.com>
2021-03-02 09:49:12 +0800

    demo

--- src/test/java/com/xsw/activitiapi/simplecase/Demo13.java
+++ src/test/java/com/xsw/activitiapi/simplecase/Demo13.java
@@ -4,6 +4,7 @@ import com.xsw.activitiapi.utils.AESHelper;
 import com.xsw.activitiapi.utils.DateToLong;
 import com.xsw.activitiapi.utils.HttpUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 /**
@@ -17,21 +18,20 @@ public class Demo13 {
     private static String secretKey = "";
     private static String signal = "";
 
-    private static String startTime = "2021-02-01 00:00:00";
-    private static String endTime = "2021-02-17 00:00:00";
+    private static String startTime = "2021-02-26 00:00:00";
+    private static String endTime = "2021-02-27 00:00:00";
 
     private static String ivS = "426e26e82c704e59";
-    private static String tokenUrl = "http:///auth_v1/auth_token/query/getWanAccessToken";
-    private static String dataUrl = "http:///zeta_v1/wan_ms/query/" + deviceId + "/getMsUploadDataByDate?access_token=";
+    private static String tokenUrl = "/auth_v1/auth_token/query/getWanAccessToken";
+    private static String dataUrl = "zeta_v1/wan_ms/query/" + deviceId + "/getAppDataByDate?access_token=";
 
 
     public static void main(String[] args) {
-        String token = "";
         String tokenParam = "api_key=" + apiKey + "&signal=" + signal;
 
         String tokenJson = HttpUtils.sendGet(tokenUrl, tokenParam);
         String[] tokenJsonArray = tokenJson.split("\"");
-        token = tokenJsonArray[5];
+        String token = tokenJsonArray[5];
         System.out.println("token:" + token);
 
         Date start = DateToLong.convertStringToDate(startTime);
@@ -40,17 +40,17 @@ public class Demo13 {
         System.out.println("startTime:" + DateToLong.convertDataToLong(start));
         System.out.println("endTime:" + DateToLong.convertDataToLong(end));
 
-        String content = "{\"starttime\": " + DateToLong.convertDataToLong(start) + ",\"endtime\": " + DateToLong.convertDataToLong(end) + "}";
+        String content = "{\"startTime\": " + DateToLong.convertDataToLong(start) + ",\"endTime\": " + DateToLong.convertDataToLong(end) + "}";
 
         try {
-            content = AESHelper.Encrypt(content, secretKey.getBytes("utf8"), ivS.getBytes("utf8"));
+            content = AESHelper.Encrypt(content, secretKey.getBytes(StandardCharsets.UTF_8), ivS.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println("Encrypt:" + content);
 
         try {
-            System.out.println("Decrypt:" + AESHelper.Decrypt(content, secretKey.getBytes("utf8")));
+            System.out.println("Decrypt:" + AESHelper.Decrypt(content, secretKey.getBytes(StandardCharsets.UTF_8)));
         } catch (Exception e) {
             e.printStackTrace();
         }
