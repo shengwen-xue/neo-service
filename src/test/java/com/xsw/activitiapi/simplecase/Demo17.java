fb62ccb4e6c32e0d490a2a8794459de5bcf1df70
xueshengwen <xueshengwen@aliyun.com>
2021-03-16 18:09:14 +0800

    format code

--- src/test/java/com/xsw/activitiapi/simplecase/Demo17.java
+++ src/test/java/com/xsw/activitiapi/simplecase/Demo17.java
@@ -1,11 +1,12 @@
 package com.xsw.activitiapi.simplecase;
 
+import com.google.common.primitives.Ints;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author xueshengwen
@@ -15,7 +16,7 @@ public class Demo17 {
 
     public static void main(String[] args) {
 
-        List<Integer> ages = Arrays.asList(1,2,1,1,1,2,3,4,4,4,4);
+        List<Integer> ages = Arrays.asList(1, 2, 1, 1, 1, 2, 3, 4, 4, 4, 4);
         List<Integer> stream = ages.stream().distinct().collect(Collectors.toList());
         LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>(ages);
         ArrayList<Integer> list = new ArrayList<>(linkedHashSet);
@@ -24,5 +25,13 @@ public class Demo17 {
         System.out.println(stream);
         System.out.println(linkedHashSet);
         System.out.println(list);
+
+        int[] numbers = {1, 2, 3, 2, 2, 3, 4, 5, 8, 8, 9};
+        List<Integer> numberList = Ints.asList(numbers);
+        numberList.set(0, 100);
+        LinkedHashSet<Integer> numberSet = new LinkedHashSet<>(numberList);
+
+        System.out.println(numberList);
+        System.out.println(numberSet);
     }
 }
