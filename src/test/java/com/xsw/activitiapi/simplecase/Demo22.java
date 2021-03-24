acce84fefea105bcff29fdef82394415ebf99aac
xueshengwen <xueshengwen@aliyun.com>
2021-03-24 17:58:28 +0800

    demo22

---
+++ src/test/java/com/xsw/activitiapi/simplecase/Demo22.java
@@ -0,0 +1,14 @@
+package com.xsw.activitiapi.simplecase;
+
+/**
+ * @author xueshengwen
+ * @since 2021/3/24 15:09
+ */
+public class Demo22 {
+
+    public static void main(String[] args) {
+        String str = "111";
+        str = "222";
+        System.out.println(str);
+    }
+}
