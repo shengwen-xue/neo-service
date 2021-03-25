9c143675734270dd5692a8fb2fa8aa502a5a05ca
xueshengwen <xueshengwen@aliyun.com>
2021-03-25 18:05:18 +0800

    demo22

--- src/test/java/com/xsw/activitiapi/simplecase/Demo22.java
+++ src/test/java/com/xsw/activitiapi/simplecase/Demo22.java
@@ -12,12 +12,6 @@ import java.util.Map;
 public class Demo22 {
 
     public static void main(String[] args) {
-        String url = "http://localhost:9098/dataservice/alarm/collectAlarmData";
-        Map<String, String> map = new HashMap<>();
-        map.put("deviceId", "4f0094e3");
-        map.put("endDate", "2021-03-25 17:30:00");
-        map.put("startDate", "2021-03-25 00:00:00");
-        String result = HttpUtils.sendPost(url, map);
-        System.out.println(result);
+
     }
 }
