b833a0f5e6da0aaac8b3cb36e44e11babed64408
xueshengwen <xueshengwen@aliyun.com>
2021-03-25 17:44:57 +0800

    demo22

--- src/main/java/com/xsw/activitiapi/utils/HttpUtils.java
+++ src/main/java/com/xsw/activitiapi/utils/HttpUtils.java
@@ -1,9 +1,7 @@
 package com.xsw.activitiapi.utils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -122,6 +120,70 @@ public class HttpUtils {
             }
         }
         return result;
+    }
+
+    public static String sendPost(String url, Map<String, String> params) {
+        OutputStreamWriter out = null;
+        BufferedReader in = null;
+        StringBuilder result = new StringBuilder();
+        try {
+            URL realUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) realUrl.openConnection();
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            // POST方法
+            conn.setRequestMethod("POST");
+            // 设置通用的请求属性
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.connect();
+            // 获取URLConnection对象对应的输出流
+            out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+            // 发送请求参数
+            if (params != null) {
+                StringBuilder param = new StringBuilder();
+                for (Map.Entry<String, String> entry : params.entrySet()) {
+                    if (param.length() > 0) {
+                        param.append("&");
+                    }
+                    param.append(entry.getKey());
+                    param.append("=");
+                    param.append(entry.getValue());
+                    System.out.println(entry.getKey() + ":" + entry.getValue());
+                }
+                System.out.println("param:" + param.toString());
+                out.write(param.toString());
+            }
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //使用finally块来关闭输出流、输入流
+        finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return result.toString();
     }
 }
 
--- src/test/java/com/xsw/activitiapi/simplecase/Demo22.java
+++ src/test/java/com/xsw/activitiapi/simplecase/Demo22.java
@@ -1,5 +1,10 @@
 package com.xsw.activitiapi.simplecase;
 
+import com.xsw.activitiapi.utils.HttpUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author xueshengwen
  * @since 2021/3/24 15:09
@@ -7,8 +12,12 @@ package com.xsw.activitiapi.simplecase;
 public class Demo22 {
 
     public static void main(String[] args) {
-        String str = "111";
-        str = "222";
-        System.out.println(str);
+        String url = "http://localhost:9098/dataservice/alarm/collectAlarmData";
+        Map<String, String> map = new HashMap<>();
+        map.put("deviceId", "4f0094e3");
+        map.put("endDate", "2021-03-25 17:30:00");
+        map.put("startDate", "2021-03-25 00:00:00");
+        String result = HttpUtils.sendPost(url, map);
+        System.out.println(result);
     }
 }
