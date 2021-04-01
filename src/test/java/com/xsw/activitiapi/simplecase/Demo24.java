5b0a58d9b9fe3804e7479d4ae6dac5f2875188a7
xueshengwen <xueshengwen@aliyun.com>
2021-04-01 15:37:17 +0800

    demo23、24

---
+++ src/test/java/com/xsw/activitiapi/simplecase/Demo24.java
@@ -0,0 +1,16 @@
+package com.xsw.activitiapi.simplecase;
+
+import com.xsw.activitiapi.model.entity.Student;
+
+/**
+ * @author xueshengwen
+ * @since 2021/4/1 14:05
+ */
+public class Demo24 implements MyInterface<Student> {
+
+    @Override
+    public Student test() {
+        return null;
+    }
+}
+
---
+++ src/test/java/com/xsw/activitiapi/simplecase/MyInterface.java
@@ -0,0 +1,10 @@
+package com.xsw.activitiapi.simplecase;
+
+/**
+ * @author xueshengwen
+ * @since 2021/4/1 14:05
+ */
+public interface MyInterface<V> {
+
+    V test();
+}
