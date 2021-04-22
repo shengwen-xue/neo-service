ac1c6d66399cea099123b4c64657a64b16a66bae
xueshengwen <xueshengwen@aliyun.com>
2021-04-22 11:22:35 +0800

    Demo30

---
+++ src/test/java/com/xsw/activitiapi/simplecase/Demo30.java
@@ -0,0 +1,12 @@
+package com.xsw.activitiapi.simplecase;
+
+/**
+ * @author xueshengwen
+ * @since 2021/4/22 11:09
+ */
+public class Demo30 {
+
+    public static void main(String[] args) {
+
+    }
+}
