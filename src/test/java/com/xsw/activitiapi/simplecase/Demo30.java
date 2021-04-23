d071732f0857cce9ff354b5dda7014d6cb13ba73
xueshengwen <xueshengwen@aliyun.com>
2021-04-23 10:34:35 +0800

    Demo30 stack

--- src/test/java/com/xsw/activitiapi/simplecase/Demo30.java
+++ src/test/java/com/xsw/activitiapi/simplecase/Demo30.java
@@ -1,5 +1,7 @@
 package com.xsw.activitiapi.simplecase;
 
+import java.util.Stack;
+
 /**
  * @author xueshengwen
  * @since 2021/4/22 11:09
@@ -7,6 +9,23 @@ package com.xsw.activitiapi.simplecase;
 public class Demo30 {
 
     public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("张三");
+        stack.push("李四");
+        stack.push("王五");
 
+        System.out.println(stack);
+
+        stack.pop();
+
+        System.out.println("返回栈顶的元素，并且将该栈顶元素出栈:" + stack);
+
+//        stack.peek();
+//
+//        System.out.println("返回栈顶的元素，但不弹出该栈顶元素:" + stack);
+
+        System.out.println(stack.search("李四"));
+
+        System.out.println(stack.size());
     }
 }
