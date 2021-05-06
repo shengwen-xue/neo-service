2c6784410bdfc2215c5a4a4ab4fad81d02c6934d
xueshengwen <xueshengwen@aliyun.com>
2021-05-06 16:27:11 +0800

    单元测试代码覆盖率

---
+++ src/test/java/com/xsw/activitiapi/demo/Demo1.java
@@ -0,0 +1,12 @@
+package com.xsw.activitiapi.demo;
+
+/**
+ * @author xueshengwen
+ * @since 2021/5/6 16:26
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        // 单元测试代码覆盖率
+    }
+}
