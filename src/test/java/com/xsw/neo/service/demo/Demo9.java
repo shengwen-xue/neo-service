1bf09d7657545d5e060096976457769ec334a5a2
xueshengwen <xueshengwen@aliyun.com>
2021-06-03 16:30:42 +0800

    demo9

--- src/test/java/com/xsw/neo/service/demo/Demo9.java
+++ src/test/java/com/xsw/neo/service/demo/Demo9.java
@@ -1,5 +1,11 @@
 package com.xsw.neo.service.demo;
 
+import com.google.common.collect.Maps;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @author xueshengwen
  * @since 2021/6/3 14:58
@@ -8,5 +14,15 @@ public class Demo9 {
 
     public static void main(String[] args) {
 
+        String url = "https://www.baidu.com/?tn={tn}";
+
+        Map<String, Object> params = Maps.newLinkedHashMap();
+        params.put("tn","baiduhome_pg");
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String forObject = restTemplate.getForObject(url, String.class, params);
+
+        System.out.println(forObject);
     }
 }
