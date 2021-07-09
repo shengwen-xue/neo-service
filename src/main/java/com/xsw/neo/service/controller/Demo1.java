336b3d1bf55409c4a3b6999a136d9e66d73b3059
xueshengwen <xueshengwen@aliyun.com>
2021-07-09 09:40:44 +0800

    test

---
+++ src/main/java/com/xsw/neo/service/controller/Demo1.java
@@ -0,0 +1,12 @@
+package com.xsw.neo.service.controller;
+
+/**
+ * @author xueshengwen
+ * @since 2021/7/9 9:40
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+    }
+}
