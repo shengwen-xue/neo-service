a61738fd35cbaa10bef3adf96f4505636b3acbb8
xueshengwen <xueshengwen@aliyun.com>
2021-07-09 09:53:41 +0800

    字符串排序

--- src/main/java/com/xsw/neo/service/controller/Demo1.java
+++ src/main/java/com/xsw/neo/service/controller/Demo1.java
@@ -1,5 +1,11 @@
 package com.xsw.neo.service.controller;
 
+import cn.hutool.core.lang.Console;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author xueshengwen
  * @since 2021/7/9 9:40
@@ -7,6 +13,13 @@ package com.xsw.neo.service.controller;
 public class Demo1 {
 
     public static void main(String[] args) {
+        List<String> list = Lists.newArrayList();
+        list.add("bbb");
+        list.add("aaa");
+        list.add("ddd");
+        list.add("ccc");
 
+        List<String> collect = list.stream().sorted().collect(Collectors.toList());
+        Console.log(collect);
     }
 }
