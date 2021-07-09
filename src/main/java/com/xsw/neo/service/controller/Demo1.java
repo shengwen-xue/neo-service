78d82254d697cd9fc06e8fdeb71ebadf97e6fa16
xueshengwen <xueshengwen@aliyun.com>
2021-07-09 10:02:06 +0800

    数值自然排序

--- src/main/java/com/xsw/neo/service/controller/Demo1.java
+++ src/main/java/com/xsw/neo/service/controller/Demo1.java
@@ -3,6 +3,7 @@ package com.xsw.neo.service.controller;
 import cn.hutool.core.lang.Console;
 import com.google.common.collect.Lists;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +14,15 @@ import java.util.stream.Collectors;
 public class Demo1 {
 
     public static void main(String[] args) {
+        List<Integer> numList = Lists.newArrayList();
+        numList.add(99);
+        numList.add(2);
+        numList.add(390);
+        numList.add(28);
+
+        List<Integer> sortedNumbers = numList.stream().sorted().collect(Collectors.toList());
+        Console.log(sortedNumbers);
+
         List<String> list = Lists.newArrayList();
         list.add("bbb");
         list.add("aaa");
