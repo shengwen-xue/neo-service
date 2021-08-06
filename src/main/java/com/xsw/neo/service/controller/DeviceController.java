af1718dae330363f20efe6ce6861655ba798989b
xueshengwen <xueshengwen@aliyun.com>
2021-08-06 13:34:28 +0800

    设备分页控制器

--- src/main/java/com/xsw/neo/service/controller/DeviceController.java
+++ src/main/java/com/xsw/neo/service/controller/DeviceController.java
@@ -4,9 +4,11 @@ import com.xsw.neo.service.common.result.ResultBody;
 import com.xsw.neo.service.service.DeviceService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,5 +27,14 @@ public class DeviceController {
     @GetMapping(value = "/batchSave")
     public ResultBody batchSave() {
         return ResultBody.SUCCESS(deviceService.batchSave());
+    }
+
+    @ApiOperation(value = "分页设备列表")
+    @GetMapping(value = "/listDevice")
+    public ResultBody listDevice(@RequestParam(value = "pageNum", required = false)
+                                 @ApiParam(value = "起始页", name = "pageNum", required = true) int pageNum,
+                                 @RequestParam(value = "pageSize", required = false)
+                                 @ApiParam(value = "页大小", name = "pageSize", required = true) int pageSize) {
+        return ResultBody.SUCCESS(deviceService.listDevice(pageNum, pageSize));
     }
 }
